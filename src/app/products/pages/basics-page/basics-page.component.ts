1b6c6617bec3233e7ba39c7698ba380a531bfdd6
Sergio <sergio.ortiz@vipera.com>
2023-11-17 12:40:44 +0100

    Fin de la seccion 11

---
+++ src/app/products/pages/basics-page/basics-page.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-basics-page',
+  templateUrl: './basics-page.component.html',
+  styleUrls: ['./basics-page.component.css']
+})
+export class BasicsPageComponent {
+  public nameLower: string = 'sergio';
+  public nameUpper: string = 'SERGIO';
+  public fullName: string = 'sErGiO oRtIz';
+
+  public customDate: Date = new Date();
+}
---
+++ src/app/products/pages/uncommon-page/uncommon-page.component.ts
@@ -0,0 +1,53 @@
+import { Component } from '@angular/core';
+import {interval, tap} from "rxjs";
+
+@Component({
+  selector: 'app-uncommon-page',
+  templateUrl: './uncommon-page.component.html',
+  styleUrls: ['./uncommon-page.component.css']
+})
+export class UncommonPageComponent {
+  //i18nSelect
+  public name: string = 'Sergio';
+  public gender: string = 'male';
+  public invitationMap = {
+    male: 'invitarlo',
+    female: 'invitarla'
+  }
+
+  changeClient(): void {
+    this.name = 'Melissa';
+    this.gender = 'female';
+  }
+
+  //i18nPlural & slice
+  public clients: string[] = ['María', 'Pedro', 'Fernando', 'Sergio', 'Eduardo', 'Melissa', 'Natalia'];
+  public clientsMap = {
+    '=0': 'no tenemos ningún cliente esperando',
+    '=1': 'tenemos un cliente esperando',
+    'other': 'tenemos # clientes esperando'
+  }
+
+  deleteClient(): void {
+    this.clients.shift();
+  }
+
+  //Json & KeyValue
+  public person = {
+    name: 'Sergio',
+    age: 23,
+    address: 'Madrid'
+  }
+
+  //Async
+  public myObservableTimer = interval(2000).pipe(
+    tap(value => console.log('tap', value))
+  );
+
+  public promiseValue = new Promise(resolve => {
+    setTimeout(() => {
+      resolve('Tenemos data en la promesa');
+    }, 3500);
+  })
+
+}
